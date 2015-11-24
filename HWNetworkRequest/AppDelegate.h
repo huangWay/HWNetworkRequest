81c736dee4f8814d2ef1f502921622bb2812b9d9
HuangWay <hw758214335@163.com>
2015-11-24 13:25:58 +0800

    Initial Commit

---
+++ HWNetworkRequest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HWNetworkRequest
+//
+//  Created by HuangWay on 15/11/24.
+//  Copyright © 2015年 HuangWay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HWNetworkRequest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HWNetworkRequest
+//
+//  Created by HuangWay on 15/11/24.
+//  Copyright © 2015年 HuangWay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
