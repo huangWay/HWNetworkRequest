0cc32625cf8cd866e936ab18dca00618ae19c14d
HuangWay <hw758214335@163.com>
2015-11-27 23:50:11 +0800

    fan封装

--- HWNetworkRequest/ViewController.m
+++ HWNetworkRequest/ViewController.m
@@ -9,7 +9,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property(nonatomic,strong) UIScrollView *scroll;
+@property(nonatomic,strong) UIView *rotationView;
 @end
 
 @implementation ViewController
@@ -18,7 +19,26 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
+- (void)setUpUI {
+    self.rotationView = [[UIView alloc]init];
+    self.rotationView.frame = CGRectMake(50, 50, 200, 200);
+    for (NSInteger i = 0; i < 4; i ++) {
+        NSString *name = [NSString stringWithFormat:@"share%ld",(long)i];
+        UIButton *btn = [self buttonWithImageName:name action:@selector(buttonClick:) tag:i];
+        [self.rotationView addSubview:btn];
+    }
+}
 
+- (UIButton *)buttonWithImageName:(NSString *)name action:(SEL)action tag:(NSInteger)tag {
+    UIButton *btn = [[UIButton alloc]init];
+    [btn setBackgroundImage:[UIImage imageNamed:name] forState:UIControlStateNormal];
+    [btn addTarget:self action:action forControlEvents:UIControlEventTouchUpInside];
+    btn.tag = tag;
+    return btn;
+}
+- (void)buttonClick:(UIButton *)sender {
+    
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
